9db25af50cdf563454bbc3ed2f871396b1f54526
Edwige08 <edwige.saves@gmail.com>
2025-04-05 10:42:40 +0200

    option 1

---
+++ adalicious.sql
@@ -0,0 +1,37 @@
+-- Dessin de la base de données fait sur drawSQL : 
+-- https://drawsql.app/teams/-3574/diagrams/adalicious
+
+-- Création table dishes :
+CREATE TABLE dishes (
+    id INTEGER PRIMARY KEY AUTOINCREMENT,
+    dish_name TEXT NOT NULL,
+    price INTEGER NOT NULL
+);
+
+-- Création table orders qui comprend la FK vers dishes :
+CREATE TABLE orders (
+    id INTEGER PRIMARY KEY AUTOINCREMENT,
+    dish_id INTEGER,
+    created_at DATETIME NOT NULL,
+    FOREIGN KEY(dish_id) REFERENCES dishes(id) 
+);
+
+-- Ajout de plats dans dishes :
+INSERT INTO dishes (dish_name, price) VALUES
+    ('Hello World Burger', 10),
+    ('404 Not Found Fries', 4),
+    ('JSON Nuggets', 8),
+    ('HTTPS Milkshake', 6);
+
+-- Ajout de commandes dans orders : 
+INSERT INTO orders (dish_id, created_at) VALUES
+    (1, '2025-04-05 11:55');
+
+INSERT INTO orders (dish_id, created_at) VALUES
+    (3, '2025-04-05 12:02');
+
+INSERT INTO orders (dish_id, created_at) VALUES
+    (2, '2025-04-05 12:04');
+
+INSERT INTO orders (dish_id, created_at) VALUES
+    (4, '2025-04-05 12:05');
